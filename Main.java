e8dc1087c02c74bf600e83e6819a10e3e398e9d6
Eduarda Portel <eduarda.portel4@gmail.com>
2024-05-29 00:18:15 -0300

    queue interface + LikedList

--- Main.java
+++ Main.java
@@ -1,2 +1,15 @@
-// queue : FIFO data structure, first in first out
-// ex: linear data structure
+import java.util.Queue;
+import java.util.LinkedList;
+
+// queue : FIFO data structure, ex: a line of people
+// linear data structure
+
+public class Main {
+    public static void main(String[] args) {
+
+        // queue is a template/interface and we need a class to apply it
+        // 2 types of class: LinkedList and PriorityQueue
+        Queue<String> queue = new LinkedList<String>();
+
+    };
+}
