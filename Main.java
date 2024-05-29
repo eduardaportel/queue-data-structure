aac913b4bb3ed79f0b95d208ba72f78db82a5ee3
Eduarda Portel <eduarda.portel4@gmail.com>
2024-05-29 00:27:25 -0300

    add elements to queue

--- Main.java
+++ Main.java
@@ -9,7 +9,17 @@ public class Main {
 
         // queue is a template/interface and we need a class to apply it
         // 2 types of class: LinkedList and PriorityQueue
+
+        // initialize a queue with LinkedList
         Queue<String> queue = new LinkedList<String>();
+
+        // add = enqueue, offer()
+        queue.offer("Karen");
+        queue.offer("Ms. Jackson");
+        queue.offer("Steve");
+        queue.offer("Harold");
+        
+        System.out.println(queue);
 
     };
 }
