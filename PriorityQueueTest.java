e0fb14d0381bed5d5885bf14d8f11c584d10ed8f
Eduarda Portel <eduarda.portel4@gmail.com>
2024-06-06 19:08:43 -0300

    priority queue test implemented

--- PriorityQueueTest.java
+++ PriorityQueueTest.java
@@ -1,11 +1,15 @@
 import java.util.*;
-public class PriorityQueue {
+
+public class PriorityQueueTest {
     public static void main(String[] args) {
         // queue is a interface only
         // Priority Queue = FIFO data structure
+        
+        // equeue = offer()
+        // dequeue = poll()
 
         // creating a queue that receive double numbers using a Linkedlist
-        Queue<Double> queue = new LinkedList<Double>();
+        Queue<Double> queue = new PriorityQueue<>();
 
         // offer() to equeue
         queue.offer(3.4);
@@ -13,6 +17,11 @@ public class PriorityQueue {
         queue.offer(4.6);
         queue.offer(1.0);
         queue.offer(1.2);
+
+        // display elements with while() loop
+        while (!queue.isEmpty()) {
+            System.out.println(queue.poll()); //FIFO
+        }
 
     }
 }
